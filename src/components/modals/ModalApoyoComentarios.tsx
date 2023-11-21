341779caf812efc6cd83ea90711f28f6865b5a3a
Federico Ariel Gomez <54158552+Fede-Gomez@users.noreply.github.com>
2023-11-21 19:03:50 -0300

    agregado terminos y condiciones + subida versionCode y versionName

--- src/components/modals/ModalApoyoComentarios.tsx
+++ src/components/modals/ModalApoyoComentarios.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Linking, Text, View } from 'react-native';
+import { Button, Linking, Text, TouchableOpacity, View } from 'react-native';
 import Modal from "react-native-modal";
 
 
@@ -8,16 +8,22 @@ const ModalApoyoComentarios = ({isModalVisible, toggleModal}) => {
     const email = 'fedekingdom@gmail.com';
     const subject = 'Consulta sobre digiTCG HUB';
     const body = 'Hola, tengo una pregunta...';
-  
+    const urlTerminosYCondiciones = 'https://www.privacypolicies.com/live/e199edf7-c57c-43b6-8fc2-89340f5714be';
+    
     const abrirEmail = () => {
       const url = `mailto:${email}?subject=${subject}&body=${body}`;
       Linking.openURL(url)
         .catch((err) => console.error('Error al abrir el correo', err));
     };
-    const abrirEnlace = () => {
+    const abrirCafecito = () => {
         Linking.openURL(urlCafecito)
           .catch((err) => console.error('Error al abrir el enlace', err));
       };
+    
+    const abrirTerminosYCondiciones = () => {
+      Linking.openURL(urlTerminosYCondiciones)
+        .catch((err) => console.error('Error al abrir el enlace', err));
+    };
 
   return (
     <Modal isVisible={isModalVisible}>
@@ -26,7 +32,7 @@ const ModalApoyoComentarios = ({isModalVisible, toggleModal}) => {
           <Text style={{color:'white', fontSize:30}}>Dame una mano para que siga creciendo. ¡Hacé una donación y bancanos en el desarrollo!</Text> 
           <Button
             title='Apoya el proyecto'
-            onPress={abrirEnlace}
+            onPress={abrirCafecito}
             color={'green'}
           />
           <Text style={{color:'white', fontSize:30, marginTop:100}}>Si tenés problemas con la app o faltan cartas, contactame!</Text>
@@ -37,6 +43,11 @@ const ModalApoyoComentarios = ({isModalVisible, toggleModal}) => {
           />
         </View>
         <Text style={{textAlign:'center', color:'white', marginBottom:15, fontSize:13}}>Esta aplicación NO es oficial. No está producida, respaldada, apoyada o afiliada a BANDAI</Text>
+        <TouchableOpacity
+          onPress={abrirTerminosYCondiciones}
+        >
+          <Text style={{textAlign:'center', color:'white', marginBottom:15, fontSize:13}}>Terminos y condiciones</Text>
+        </TouchableOpacity>
           <Button title="Salir" onPress={toggleModal} />
       </Modal>
   );
