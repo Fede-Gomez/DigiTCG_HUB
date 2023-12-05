9d91ce2789d62d1545f8a196889222e40c21ebd1
Federico Ariel Gomez <54158552+Fede-Gomez@users.noreply.github.com>
2023-12-05 12:27:34 -0300

    pull de master

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,10 @@ import CardDigimon from "./cards/CardDigimon";
 import Folder from './folders/Folder';
 import { CardListCardsSelected, CardListCardsView, CardListMyDeck } from './cardList'
 import { ModalApoyoComentarios, ModalCard, ModalFilter, ModalAyuda} from './modals'
-import { BtnAddRemoveCards, BtnShareCards, BtnsHeadersCard, BtnsHeadersCardSelecteds, BtnChangeNameDeck} from './buttons'
+import { BtnAddRemoveCards, BtnShareCards, BtnsHeadersCard, BtnsHeadersCardSelecteds, BtnChangeNameDeck, BtnImportDeck, BtnsExportDeck} from './buttons'
 import { ErrorMessage, SuccesMessage, ErrorSound} from './alerts'
 import ModalContadorMemoria from './modals/ModalContadorMemoria';
+import { FlowChart, QuestionAnswer } from './faqs'
 
 export {
     CardDigimon,
